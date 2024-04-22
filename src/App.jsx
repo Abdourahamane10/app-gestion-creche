38de4e6b66258376b2a9c8f5635b53c906ffafcf
Abdourahamane <amdroudjae@gmail.com>
2024-04-23 00:41:55 +0200

    Mise en place d'un système de route dans le composant src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
 function App() {
   /* const [count, setCount] = useState(0) */
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
