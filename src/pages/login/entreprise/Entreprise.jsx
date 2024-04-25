fc90b64e2f3808694936483b0cdab3500c3a7298
Abdourahamane <amdroudjae@gmail.com>
2024-04-25 22:02:47 +0200

    Création du composant src/pages/login/entreprise/Entreprise.jsx et de son contenuu

---
+++ src/pages/login/entreprise/Entreprise.jsx
@@ -0,0 +1,8 @@
+export default function Entreprise() {
+  return (
+    <>
+    <label htmlFor="entreprise">Identifiant de la société</label>
+    <input id="entreprise" type="text" />
+    </>
+  )
+}
