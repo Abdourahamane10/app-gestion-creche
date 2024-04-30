b990cd23f1227b8012354c46ed5a68fd10913662
Abdourahamane <amdroudjae@gmail.com>
2024-04-30 23:55:45 +0200

    Mise en place du  contenu du composant src/pages/login/entreprise/Entreprise.jsx

--- src/pages/login/entreprise/Entreprise.jsx
+++ src/pages/login/entreprise/Entreprise.jsx
@@ -1,8 +1,9 @@
+import entrepriseStyle from "./Entreprise.module.css"
 export default function Entreprise() {
   return (
     <>
     <label htmlFor="entreprise">Identifiant de la société</label>
-    <input id="entreprise" type="text" />
+    <input id="entreprise" type="text" className={entrepriseStyle.entrepriseInput} placeholder="Identifiant de la société..." />
     </>
   )
 }
