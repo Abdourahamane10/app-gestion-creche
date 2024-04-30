96cd8a8e7d542298402b7de6b53713c978fa253b
Abdourahamane <amdroudjae@gmail.com>
2024-05-01 01:18:22 +0200

    Affichage du logo dans le composant src/pages/login/Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,18 @@
 import Service from "./service/Service";
 import Entreprise from "./entreprise/Entreprise";
+import Logo from "./logo/Logo";
 import loginStyle from "./Login.module.css"
 
 export default function Login() {
   return (
     <div className={loginStyle.loginContainer}>
-      <form className={loginStyle.loginForm}>
-        <Entreprise/>
-        <Service/>
-      </form>
+      <div className={loginStyle.logoFormContainer}>
+        <Logo/>
+        <form className={loginStyle.loginForm}>
+          <Entreprise/>
+          <Service/>
+        </form>
+      </div>
     </div>
   )
 }
