9f482bb3d7f48dd829dfb178f54b71fbbd494eb6
Abdourahamane <amdroudjae@gmail.com>
2024-04-30 23:57:26 +0200

    Affichage des composants Entreprise.jsx et Service.jsx dans le composant src/pages/login/Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,11 @@
 import Service from "./service/Service";
 import Entreprise from "./entreprise/Entreprise";
+import loginStyle from "./Login.module.css"
 
 export default function Login() {
   return (
-    <div>
-      <form>
+    <div className={loginStyle.loginContainer}>
+      <form className={loginStyle.loginForm}>
         <Entreprise/>
         <Service/>
       </form>
