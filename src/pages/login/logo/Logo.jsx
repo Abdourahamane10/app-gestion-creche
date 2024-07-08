3d4bd89f7490f5cd5d86ef54d4d5ac5ea2b5131b
Abdourahamane <amdroudjae@gmail.com>
2024-07-08 23:57:02 +0200

    Déplacement du composent Logo et de son fichier css dans le dossier loginPersonnel/logo

--- src/pages/login/logo/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import logoStyle from "./Logo.module.css"
-export default function Logo() {
-  return (
-    <div className={logoStyle.logo}>
-        <img src="/logos/PetitsChaperonsRouges.jpeg" alt="logo societe" />
-    </div>
-  )
-}
