77e85f81dfb968d68b558795f8b7ba4d270cc33a
Abdourahamane <amdroudjae@gmail.com>
2024-07-08 22:06:15 +0200

    Suppression du logo dans la page login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import Service from "./service/Service";
 import Entreprise from "./entreprise/Entreprise";
-import Logo from "./logo/Logo";
+//import Logo from "./logo/Logo";
 import loginStyle from "./Login.module.css";
 import ConnexionBtn from "../../components/boutons/ConnexionBtn";
 
@@ -8,7 +8,7 @@ export default function Login() {
   return (
     <div className={loginStyle.loginContainer}>
       <div className={loginStyle.logoFormContainer}>
-        <Logo/>
+        {/* <Logo/> */}
         <form className={loginStyle.loginForm}>
           <Entreprise/>
           <Service/>
