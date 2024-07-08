81df6a73c8e95c0e1ac8808eaf5ba98cbbe089c2
Abdourahamane <amdroudjae@gmail.com>
2024-07-08 23:52:17 +0200

    Affichage du composent LoginPersonnel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/login/Login"
+import LoginPersonnel from "./pages/loginPersonnel/LoginPersonnel"
+
 function App() {
   /* const [count, setCount] = useState(0) */
 
@@ -8,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path="/login" element={<LoginPersonnel/>}/>
       </Routes>
       </BrowserRouter>
     </>
