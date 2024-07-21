355e40f68736f634502f6958c5f0e3fe9d79ce6c
Abdourahamane <amdroudjae@gmail.com>
2024-07-21 23:40:57 +0200

    Renommage du composant InputConnexion en IdentifiantPersonnel et du fichier InputConnexion.module.css en IdentifiantPersonnel.module.css et du document inputConnexion en identifiantPersonnel

--- src/pages/loginPersonnel/inputConnexion/InputConnexion.jsx
+++
@@ -1,9 +0,0 @@
-import inputConnexionStyle from "./InputConnexion.module.css"
-export default function InputConnexion() {
-  return (
-    <>
-        <label htmlFor="identifiant_personnel">Identifiant personnel</label>
-        <input id="identifiant_personnel" type="text" className={inputConnexionStyle.identifiant_personnel} placeholder="Identifiant personnel"/>
-    </>
-  )
-}
