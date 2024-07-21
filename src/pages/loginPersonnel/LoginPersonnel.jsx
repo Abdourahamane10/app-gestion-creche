62838247e9c5d50141e149e8ab7d7830425ca3d0
Abdourahamane <amdroudjae@gmail.com>
2024-07-21 23:43:59 +0200

    Affichage du composant IdentifiantPersonnel à la place du composant InputConnexion

--- src/pages/loginPersonnel/LoginPersonnel.jsx
+++ src/pages/loginPersonnel/LoginPersonnel.jsx
@@ -1,14 +1,14 @@
 import Logo from "./logo/Logo";
 import loginPersonnelStyle from "./LoginPersonnel.module.css"
-import InputConnexion from "./inputConnexion/InputConnexion";
 import ConnexionBtn from "../../components/boutons/ConnexionBtn";
+import IdentifiantPersonnel from "./indentifiantPersonnel/IdentifiantPersonnel";
 export default function LoginPersonnel() {
   return (
     <div className={loginPersonnelStyle.loginContainer}>
       <div className={loginPersonnelStyle.loginFormContainer}>
         <Logo/>
         <form className={loginPersonnelStyle.loginForm}>
-        <InputConnexion/>
+        <IdentifiantPersonnel/>
         <ConnexionBtn/>
         </form>
       </div>
