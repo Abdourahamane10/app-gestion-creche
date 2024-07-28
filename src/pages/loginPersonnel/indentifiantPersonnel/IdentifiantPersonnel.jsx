882d2f9a63e7f63fd191da4c2322781932691239
Abdourahamane <amdroudjae@gmail.com>
2024-07-28 15:31:57 +0200

    Ajout de l'attribut ref, d'un paramètre pour la fonction et de la validation des props

--- src/pages/loginPersonnel/indentifiantPersonnel/IdentifiantPersonnel.jsx
+++ src/pages/loginPersonnel/indentifiantPersonnel/IdentifiantPersonnel.jsx
@@ -1,9 +1,14 @@
 import inputConnexionStyle from "./IdentifiantPersonnel.module.css"
-export default function IdentifiantPersonnel() {
+import PropTypes from 'prop-types'
+export default function IdentifiantPersonnel({identifiantPersonnelRef}) {
   return (
     <>
         <label htmlFor="identifiant_personnel">Identifiant</label>
-        <input id="identifiant_personnel" type="text" className={inputConnexionStyle.identifiant_personnel} placeholder="Identifiant personnel..."/>
+        <input id="identifiant_personnel" ref={identifiantPersonnelRef} type="text" className={inputConnexionStyle.identifiant_personnel} placeholder="Identifiant personnel..."/>
     </>
   )
 }
+
+IdentifiantPersonnel.propTypes = {
+  identifiantPersonnelRef: PropTypes.shape({ current: PropTypes.any }).isRequired,
+};
