4c450595a7ce4e9dff3219473619fc5d84adcc64
Abdourahamane <amdroudjae@gmail.com>
2024-07-28 12:44:43 +0200

    Affichage d'un message d'erreur lorsqu'un champ n'est pas renseigné lors de l'envoi du formulaire

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,15 +3,33 @@ import Entreprise from "./entreprise/Entreprise";
 //import Logo from "./logo/Logo";
 import loginStyle from "./Login.module.css";
 import ConnexionBtn from "../../components/boutons/ConnexionBtn";
+import { useRef } from "react";
+import { useState } from "react";
 
 export default function Login() {
+
+  const identifiantEntrepriseRef = useRef();
+  const codeServiceRef = useRef();
+  const [identifiantEntreprise, setIdentifiantEntreprise] = useState("");
+  const [codeService, setCodeService] = useState("");
+  const [isClickedSubmit, setIsClickedSubmit] = useState(false);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setIdentifiantEntreprise(identifiantEntrepriseRef.current.value);
+    setCodeService(codeServiceRef.current.value);
+    setIsClickedSubmit(true);
+  }
+
   return (
     <div className={loginStyle.loginContainer}>
       <div className={loginStyle.loginFormContainer}>
         {/* <Logo/> */}
-        <form className={loginStyle.loginForm}>
-          <Entreprise/>
-          <Service/>
+        <form className={loginStyle.loginForm} onSubmit={(e) => handleSubmit(e)}>
+          <Entreprise identifiantEntrepriseRef={identifiantEntrepriseRef} />
+          {(!identifiantEntreprise && isClickedSubmit) && (<p className={loginStyle.errorValidation}>Champ obligatoire!</p>)}
+          <Service codeServiceRef={codeServiceRef} />
+          {(!codeService && isClickedSubmit) && (<p className={loginStyle.errorValidation}>Champ obligatoire!</p>)}
           <ConnexionBtn/>
         </form>
       </div>
