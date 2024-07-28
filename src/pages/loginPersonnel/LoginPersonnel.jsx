8da934496a5d5dbf47564648461d9b2d8db335c6
Abdourahamane <amdroudjae@gmail.com>
2024-07-28 15:34:26 +0200

    Affichage de message d'erreur pour les champs non renseignés lors de l'envoi du formulaire

--- src/pages/loginPersonnel/LoginPersonnel.jsx
+++ src/pages/loginPersonnel/LoginPersonnel.jsx
@@ -3,14 +3,31 @@ import loginPersonnelStyle from "./LoginPersonnel.module.css"
 import ConnexionBtn from "../../components/boutons/ConnexionBtn";
 import IdentifiantPersonnel from "./indentifiantPersonnel/IdentifiantPersonnel";
 import PasswordPersonnel from "./passwordPersonnel/PasswordPersonnel";
+import { useRef, useState } from "react";
+
 export default function LoginPersonnel() {
+  const [identifiantPersonnel, setIdentifiantPersonnel] = useState("");
+  const [passwordPersonnel, setPasswordPersonnel] = useState("");
+  const [formSubmited, setFormSubmited] = useState(false);
+  const identifiantPersonnelRef = useRef();
+  const passwordPersonnelRef = useRef();
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setIdentifiantPersonnel(identifiantPersonnelRef.current.value);
+    setPasswordPersonnel(passwordPersonnelRef.current.value);
+    setFormSubmited(true);
+  }
+
   return (
     <div className={loginPersonnelStyle.loginContainer}>
       <div className={loginPersonnelStyle.loginFormContainer}>
         <Logo/>
-        <form className={loginPersonnelStyle.loginForm}>
-        <IdentifiantPersonnel/>
-        <PasswordPersonnel/>
+        <form className={loginPersonnelStyle.loginForm} onSubmit={(e) => handleSubmit(e)}>
+        <IdentifiantPersonnel identifiantPersonnelRef={identifiantPersonnelRef} />
+        {(!identifiantPersonnel && formSubmited) && (<p className={loginPersonnelStyle.errorValidation}>Champ obligatoire!</p>)}
+        <PasswordPersonnel passwordPersonnelRef={passwordPersonnelRef} />
+        {(!passwordPersonnel && formSubmited) && (<p className={loginPersonnelStyle.errorValidation}>Champ obligatoire!</p>)}
         <ConnexionBtn/>
         </form>
       </div>
