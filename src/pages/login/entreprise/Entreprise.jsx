d8b89f3ae41aecb43ef19a5b1ffb8cdb88a9fcc0
Abdourahamane <amdroudjae@gmail.com>
2024-07-28 12:40:56 +0200

    Ajout du propTypes, de l'attribut ref pour l'input et d'un paramètre pour la fonction

--- src/pages/login/entreprise/Entreprise.jsx
+++ src/pages/login/entreprise/Entreprise.jsx
@@ -1,9 +1,16 @@
 import entrepriseStyle from "./Entreprise.module.css"
-export default function Entreprise() {
+import PropTypes from 'prop-types';
+
+export default function Entreprise({identifiantEntrepriseRef}) {
+  
   return (
     <>
-    <label htmlFor="entreprise">Identifiant de la société</label>
-    <input id="entreprise" type="text" className={entrepriseStyle.entrepriseInput} placeholder="Identifiant de la société..." />
+    <label htmlFor="entreprise" >Identifiant de la société</label>
+    <input id="entreprise" ref={identifiantEntrepriseRef} type="text" className={entrepriseStyle.entrepriseInput} placeholder="Identifiant de la société..." />
     </>
   )
 }
+
+Entreprise.propTypes = {
+  identifiantEntrepriseRef: PropTypes.object.isRequired,
+};
