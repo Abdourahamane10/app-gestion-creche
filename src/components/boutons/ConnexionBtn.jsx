c8a4e16e0f5d5a67ae3307d1deeea98319b28080
Abdourahamane <amdroudjae@gmail.com>
2024-08-30 23:50:08 +0200

    Affichage du loading et ajout des propTypes pour les props

--- src/components/boutons/ConnexionBtn.jsx
+++ src/components/boutons/ConnexionBtn.jsx
@@ -1,8 +1,17 @@
 import connectionBtnStyle from "./ConnexionBtn.module.css"
-export default function ConnexionBtn() {
+import PropTypes from 'prop-types'
+
+export default function ConnexionBtn({loading}) {
   return (
     <div className={connectionBtnStyle.containerBtnConnexion}>
-    <button className={connectionBtnStyle.connexionBtn}>Se connecter</button>
+    <button className={connectionBtnStyle.connexionBtn}>
+    {loading && (<img className={connectionBtnStyle.spinner} src="/icones/spinner.svg" />)}
+      Se connecter
+      </button>
     </div>
   )
+}
+
+ConnexionBtn.propTypes = {
+  loading: PropTypes.shape({current: PropTypes.any}),
 }
