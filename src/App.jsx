0a54bc7ec8fa609980fc7288ad20822a4f0d490a
Abdourahamane <amdroudjae@gmail.com>
2024-08-31 16:39:29 +0200

    Ajout de la route accueil

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/login/Login"
 import LoginPersonnel from "./pages/loginPersonnel/LoginPersonnel"
+import Home from "./pages/professionnel/Home"
 
 function App() {
   /* const [count, setCount] = useState(0) */
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/login" element={<LoginPersonnel/>}/>
+        <Route path="/accueil" element={<Home/>} />
       </Routes>
       </BrowserRouter>
     </>
