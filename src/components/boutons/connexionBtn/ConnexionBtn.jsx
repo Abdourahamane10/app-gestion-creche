4c37595a9e5d0cf6701d079f122072d49a240cd8
Abdourahamane <amdroudjae@gmail.com>
2024-10-08 19:30:53 +0200

    Création du dossier connexionBtn et déplacement des fichiers ConnexionBtn.jsx et ConnexionBtn.module.css dedans

---
+++ src/components/boutons/connexionBtn/ConnexionBtn.jsx
@@ -0,0 +1,17 @@
+import connectionBtnStyle from "./ConnexionBtn.module.css"
+import PropTypes from 'prop-types'
+
+export default function ConnexionBtn({loading}) {
+  return (
+    <div className={connectionBtnStyle.containerBtnConnexion}>
+    <button className={connectionBtnStyle.connexionBtn}>
+    {loading && (<img className={connectionBtnStyle.spinner} src="/icones/spinner.svg" />)}
+      Se connecter
+      </button>
+    </div>
+  )
+}
+
+ConnexionBtn.propTypes = {
+  loading: PropTypes.bool,
+}
