984ee35ace69304c55429fdb02741c441f22572d
Abdourahamane <amdroudjae@gmail.com>
2024-10-10 20:00:21 +0200

    Création du composant Deconnexion et de sa feuille de style

---
+++ src/components/boutons/deconnexionBtn/DeconnexionBtn.jsx
@@ -0,0 +1,9 @@
+import deconnexionBtnStyle from "./DeconnexionBtn.module.css"
+
+export default function DeconnexionBtn() {
+  return (
+    <>
+    <button className={deconnexionBtnStyle.deconnexionBtn}>Se déconnecter</button>
+    </>
+  )
+}
