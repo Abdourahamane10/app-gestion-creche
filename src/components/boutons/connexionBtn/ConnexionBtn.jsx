e2253c347edad0b4190e37691fef5944e9c56c5f
Abdourahamane <amdroudjae@gmail.com>
2024-11-02 17:17:30 +0100

    Importation de la feuille de style index.module.css

--- src/components/boutons/connexionBtn/ConnexionBtn.jsx
+++ src/components/boutons/connexionBtn/ConnexionBtn.jsx
@@ -1,11 +1,13 @@
-import connectionBtnStyle from "./ConnexionBtn.module.css"
 import PropTypes from 'prop-types'
+
+import connectionBtnStyle from "./ConnexionBtn.module.css"
+import indexStyle from "../../../index.module.css"
 
 export default function ConnexionBtn({loading}) {
   return (
     <div className={connectionBtnStyle.containerBtnConnexion}>
     <button className={connectionBtnStyle.connexionBtn}>
-    {loading && (<img className={connectionBtnStyle.spinner} src="/icones/spinner.svg" />)}
+    {loading && (<img className={indexStyle.spinner} style={{ color: 'blue' }} src="/icones/spinner.svg" />)}
       Se connecter
       </button>
     </div>
