e265f8af3a874602fb9f3df4d0e355b80340eced
Abdourahamane <amdroudjae@gmail.com>
2024-11-03 00:50:21 +0100

    Modification du slice authSlice

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -2,7 +2,8 @@ import {createSlice} from '@reduxjs/toolkit'
 
 
 const initialState = {
-    token: localStorage.getItem('accessToken') || null
+    token: localStorage.getItem('accessToken') || null,
+    codeUser: localStorage.getItem('codeUser') || null
 }
 
 
@@ -11,13 +12,18 @@ export const authSlice = createSlice({
     initialState,
     reducers: {
         loginReducer: (state, action) => {
-            //Quand cette action sera envoyée, on va mettre à jour le token dans le redux et dans le localStorage du navigateur
-            state.token = action.payload;
-            localStorage.setItem('accessToken', action.payload);
+            //Quand cette action sera envoyée, on va mettre à jour le token et le code_user dans le redux et dans le localStorage du navigateur
+            const {token, codeUser} = action.payload;
+            state.token = token;
+            state.codeUser = codeUser;
+            localStorage.setItem('accessToken', token);
+            localStorage.setItem('codeUser', codeUser);
         },
         logoutReducer: (state) => {
             state.token = null;
+            state.codeUser = null;
             localStorage.removeItem('accessToken');
+            localStorage.removeItem('codeUser');
         }
     }
 });
