f0ebb254015cad363162b14969734760309d0f17
Abdourahamane <amdroudjae@gmail.com>
2024-11-04 20:14:26 +0100

    Affichage dynamique de la page d'accueil correspondant à l'utilisateur connecté

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/login" element={<LoginPersonnel/>}/>
-        <Route path="/accueil" element={((codeUser == "DR" || codeUser == "DA") && (<HomeDirection />)) || ((codeUser == "PR") && (<HomeProfessionnel />))} />
+        <Route path="/accueil" element={((codeUser == "DR" || codeUser == "DA") && (<HomeDirection />)) || ((codeUser == "AP" || codeUser == "IN" || codeUser == "EJ" || codeUser == "AE") && (<HomeProfessionnel />))} />
       </Routes>
       </BrowserRouter>
     </>
