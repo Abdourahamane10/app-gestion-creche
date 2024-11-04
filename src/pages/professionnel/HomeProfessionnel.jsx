b0b827e30d14d713dee8bc7c7a6b3c9a5bc28e2a
Abdourahamane <amdroudjae@gmail.com>
2024-11-04 20:32:32 +0100

    Renommage homeStyle En homeProfessionnelStyle

--- src/pages/professionnel/HomeProfessionnel.jsx
+++ src/pages/professionnel/HomeProfessionnel.jsx
@@ -1,13 +1,13 @@
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import MainProfessionnel from "../../components/mains/mainProfessionnel/MainProfessionnel";
-import homeStyle from "./HomeProfessionnel.module.css";
+import homeProfessionnelStyle from "./HomeProfessionnel.module.css";
 
 export default function HomeProfessionnel() {
   return (
-    <div className={homeStyle.home_container}>
+    <div className={homeProfessionnelStyle.home_container}>
       <Header />
-      <main className={homeStyle.main_container}>
+      <main className={homeProfessionnelStyle.main_container}>
         <MainProfessionnel />
       </main>
       <Footer />
