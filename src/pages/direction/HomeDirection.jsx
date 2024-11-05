8957a58be7031927f9d81cb69e73fa26b417e821
Abdourahamane <amdroudjae@gmail.com>
2024-11-05 22:23:50 +0100

    Modification de la fonction HomeDirection

--- src/pages/direction/HomeDirection.jsx
+++ src/pages/direction/HomeDirection.jsx
@@ -1,17 +1,9 @@
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
 import MainPrincipal from "../../components/mains/mainPrincipal/MainPrincipal";
-// import homeDirectionStyle from "./HomeDirection.module.css"
-import mainPrincipalStyle from "../../components/mains/mainPrincipal/MainPrincipal.module.css"
 
 export default function HomeDirection() {
   return (
-    <div className={mainPrincipalStyle.home_container}>
-      <Header />
-      <main className={mainPrincipalStyle.main_container}>
-        <MainPrincipal />
-      </main>
-      <Footer />
-    </div>
+    <>
+    <MainPrincipal />
+    </>
   )
 }
