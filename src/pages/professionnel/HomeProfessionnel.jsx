89b7d7ef4448327292f4d73d5398877bfcbb1126
Abdourahamane <amdroudjae@gmail.com>
2024-11-05 22:22:47 +0100

    Modification de la fonction HomeProfessionnel

--- src/pages/professionnel/HomeProfessionnel.jsx
+++ src/pages/professionnel/HomeProfessionnel.jsx
@@ -1,17 +1,9 @@
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
 import MainPrincipal from "../../components/mains/mainPrincipal/MainPrincipal";
-// import homeProfessionnelStyle from "./HomeProfessionnel.module.css";
-import mainPrincipalStyle from "../../components/mains/mainPrincipal/MainPrincipal.module.css"
 
 export default function HomeProfessionnel() {
   return (
-    <div className={mainPrincipalStyle.home_container}>
-      <Header />
-      <main className={mainPrincipalStyle.main_container}>
-        <MainPrincipal />
-      </main>
-      <Footer />
-    </div>
+    <>
+    <MainPrincipal />
+    </>
   )
 }
