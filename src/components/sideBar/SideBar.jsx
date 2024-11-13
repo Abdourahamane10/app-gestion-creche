74e42c3d6f7c83adf36b4c39d2f4188b22448cf7
Abdourahamane <amdroudjae@gmail.com>
2024-11-13 19:32:51 +0100

    Affichage d'un contenu dynamique du composant SideBar selon l'utilisateur connecté

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,20 +1,48 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 import sideBarStyle from "./SideBar.module.css";
 
 export default function SideBar() {
+
+  const codeUser = useSelector(state => state.auth.codeUser);
+
   return (
     <aside className={sideBarStyle.sideBar}>
+      {((codeUser == "DR") || (codeUser == "DA")) ? (
+        <>
+        <div className={sideBarStyle.item_direction}>
+        <Link to="#">
+          Menus
+        </Link>
+      </div>
+      <div className={sideBarStyle.item_direction}>
+        <Link to="#">
+          Activités
+        </Link>
+      </div>
+      <div className={sideBarStyle.item_direction}>
+        <Link to="#">
+          Évènements
+        </Link>
+      </div>
+        </>
+    ) : (
+      <>
       <div className={sideBarStyle.item_prof}>
-        <a href="#">
+        <Link to="#">
           <img src="PhotosProfessionnelles/Photo_defaut.jpg" alt="Photo professionnelle" />
           <p>Professionnelle1</p>
-        </a>
+        </Link>
       </div>
       <div className={sideBarStyle.item_prof}>
-        <a href="#">
+        <Link to="#">
           <img src="PhotosProfessionnelles/Photo_defaut.jpg" alt="Photo professionnelle" />
           <p>Professionnelle2</p>
-        </a>
+        </Link>
       </div>
+      </>
+    )}
     </aside>
   )
 }
