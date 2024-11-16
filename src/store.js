da1dc2f1eba2f9465230b235f4c715f3941aa7f7
Abdourahamane <amdroudjae@gmail.com>
2024-11-17 00:10:03 +0100

    ajout du slice textesAdminSlice.js dans le store store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
+
 import authReducer from './features/authSlice';
+import textesAdminReducer from './features/textesAdminSlice';
 
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        textesAdmin: textesAdminReducer
     }
 });
