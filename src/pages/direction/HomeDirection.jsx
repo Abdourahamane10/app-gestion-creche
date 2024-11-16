e93cf3ae86e16a50c9d48cc2a198070221267c4f
Abdourahamane <amdroudjae@gmail.com>
2024-11-16 19:42:48 +0100

    Déplacement du composant HomeDirection et de la feuille de style HomeDirection.module.css dans le dossier home

--- src/pages/direction/HomeDirection.jsx
+++
@@ -1,9 +0,0 @@
-import MainPrincipal from "../../components/mains/mainPrincipal/MainPrincipal";
-
-export default function HomeDirection() {
-  return (
-    <>
-    <MainPrincipal />
-    </>
-  )
-}
