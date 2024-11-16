fa6f96f96440642a64524a7f9404e2b6d3fc3d64
Abdourahamane <amdroudjae@gmail.com>
2024-11-17 00:14:23 +0100

    Création du slice src/features/textesAdminSlice.js

---
+++ src/features/textesAdminSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    presentationTexte: "",
+    projetPedagogiqueTexte: "",
+    reglementTexte: ""
+}
+
+export const textesAdminSlice = createSlice({
+    name: "textesAdmin",
+    initialState,
+    reducers: {
+        presentationReducer: (state, action) => {
+            const {presentationTexte} = action.payload;
+            state.presentationTexte = presentationTexte;
+        },
+        projetPedagogiqueReducer: (state, action) => {
+            const {projetPedagogiqueTexte} = action.payload;
+            state.projetPedagogiqueTexte = projetPedagogiqueTexte;
+        },
+        reglementReducer: (state, action) => {
+            const {reglementTexte} = action.payload;
+            state.reglementTexte = reglementTexte;
+        }
+    }
+});
+
+export const {presentationReducer, projetPedagogiqueReducer, reglementReducer} = textesAdminSlice.actions;
+
+export default textesAdminSlice.reducer;
