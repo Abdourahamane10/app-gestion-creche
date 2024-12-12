8c8dc1cf33fe6d89700da235b81095e98b7efd43
Abdourahamane <amdroudjae@gmail.com>
2024-12-12 21:27:21 +0100

    Appel de l'API index à la place de l'API show et modification de la couleur de fond des icônes de chargement

--- src/components/mains/mainPrincipal/MainPrincipal.jsx
+++ src/components/mains/mainPrincipal/MainPrincipal.jsx
@@ -27,8 +27,8 @@ export default function MainPrincipal() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    setAPIState(prevState => ({ ...prevState, loading: true }));
-    fetch("http://127.0.0.1:8000/api/parametresGeneraux/1",{
+    setAPIState({loading: true, error: false, data: undefined});
+    fetch("http://127.0.0.1:8000/api/parametresGeneraux",{
       method: "GET",
       headers: {
         'Content-Type': 'application/json',
@@ -48,9 +48,9 @@ export default function MainPrincipal() {
     })
     .then(responseData => {
       setAPIState({loading: false, error: false, data: responseData});
-      const text_presentation = responseData.data.texte_presentation
-      const text_projetPedagogique = responseData.data.texte_projet_pedagogique;
-      const text_reglementInterieur = responseData.data.texte_reglement_interieur;
+      const text_presentation = responseData.data[0].presentation
+      const text_projetPedagogique = responseData.data[0].projet_pedagogique;
+      const text_reglementInterieur = responseData.data[0].reglement_interieur;
       dispatch(presentationReducer(text_presentation));
       dispatch(projetPedagogiqueReducer(text_projetPedagogique));
       dispatch(reglementReducer(text_reglementInterieur));
@@ -85,7 +85,7 @@ export default function MainPrincipal() {
         {presentationTexte != "" && (
           <div className={mainProfessionnnelStyle.presentation_container}>
           <h2>Présentation de la crèche</h2>
-          {APIState.loading && (<img className={indexStyle.spinner} style={{ color: 'blue' }} src="/icones/spinner.svg" />)}
+          {APIState.loading && (<img className={indexStyle.spinner} style={{ backgroundColor: 'gray' }} src="/icones/spinner.svg" />)}
           <p>{presentationTexte}</p>
           {((codeUser == "DR") || (codeUser == "DA")) && (
             <div className={mainProfessionnnelStyle.btnModifierContainer}>
@@ -96,7 +96,7 @@ export default function MainPrincipal() {
         {projetPedagogiqueTexte != "" && (
           <div className={mainProfessionnnelStyle.pedagogique_container}>
           <h2>Projet pédagogique de la crèche</h2>
-          {APIState.loading && (<img className={indexStyle.spinner} style={{ color: 'blue' }} src="/icones/spinner.svg" />)}
+          {APIState.loading && (<img className={indexStyle.spinner} style={{ backgroundColor: 'gray' }} src="/icones/spinner.svg" />)}
           <p>{projetPedagogiqueTexte}</p>
           {((codeUser == "DR") || (codeUser == "DA")) && (
             <div className={mainProfessionnnelStyle.btnModifierContainer}>
@@ -107,7 +107,7 @@ export default function MainPrincipal() {
         {reglementTexte != "" && (
           <div className={mainProfessionnnelStyle.reglement_container}>
           <h2>Réglement intérieur</h2>
-          {APIState.loading && (<img className={indexStyle.spinner} style={{ color: 'blue' }} src="/icones/spinner.svg" />)}
+          {APIState.loading && (<img className={indexStyle.spinner} style={{ backgroundColor: 'gray' }} src="/icones/spinner.svg" />)}
           <p>{reglementTexte}</p>
           {((codeUser == "DR") || (codeUser == "DA")) && (
             <div className={mainProfessionnnelStyle.btnModifierContainer}>
