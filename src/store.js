93e245445c48d8d03a3a784d5a41c6044401fae7
Abdourahamane <amdroudjae@gmail.com>
2024-12-16 19:55:14 +0100

    Ajout du slice listeSections dans le store

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit'
 
 import authReducer from './features/authSlice';
 import textesAdminReducer from './features/textesAdminSlice';
+import sectionsReducer from './features/sectionSlice';
 
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        textesAdmin: textesAdminReducer
+        textesAdmin: textesAdminReducer,
+        listeSections: sectionsReducer
     }
 });
