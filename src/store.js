eec09d97f20768c4d8be760318d8d937441c145f
Abdourahamane <amdroudjae@gmail.com>
2024-12-29 19:50:19 +0100

    Ajout du slice employes

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import {configureStore} from '@reduxjs/toolkit'
 import authReducer from './features/authSlice';
 import textesAdminReducer from './features/textesAdminSlice';
 import sectionsReducer from './features/sectionSlice';
+import employesReducer from './features/employeSlice';
 
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         textesAdmin: textesAdminReducer,
-        listeSections: sectionsReducer
+        listeSections: sectionsReducer,
+        employes:  employesReducer
     }
 });
