b2a572cef26a81085e13eba4889d83c38ce89141
Abdourahamane <amdroudjae@gmail.com>
2025-01-16 21:03:28 +0100

    Ajout d'un icône d'oeil dans le champ du mot de passe

--- src/pages/loginPersonnel/passwordPersonnel/PasswordPersonnel.jsx
+++ src/pages/loginPersonnel/passwordPersonnel/PasswordPersonnel.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import passwordStyle from "./PasswordPersonnel.module.css"
 import PropTypes from 'prop-types'
 
 export default function PasswordPersonnel({passwordPersonnelRef}) {
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  function handleClickEye() {
+    setPasswordVisible(!passwordVisible);
+  }
   return (
     <>
         <label htmlFor="passwordPersonnel" className={passwordStyle.labelPassword}>Mot de passe</label>
-        <input id="passwordPersonnel" ref={passwordPersonnelRef} type="password" className={passwordStyle.password_input} placeholder="Mot de passe personnel..." />
+        <div className={passwordStyle.inputPasswordContainer}>
+          <input id="passwordPersonnel" ref={passwordPersonnelRef} type={passwordVisible ? "text" : "password"} className={passwordStyle.password_input} placeholder="Mot de passe personnel..." />
+          <span onClick={handleClickEye} className={passwordStyle.eyeIcone} aria-label={passwordVisible ? "Masquer le mot de passe" : "Afficher le mot de passe"}>{passwordVisible ? "👁️" : "👁️‍🗨️"}</span>
+      </div>
     </>
   )
 }
