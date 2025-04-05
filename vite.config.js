2b066d0edda8c1fa957e68ea833a19493d178483
Abdourahamane <amdroudjae@gmail.com>
2025-04-05 12:59:17 +0200

    Mise à jour des packages et définition d'un port pour l'environnement de dev

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,  // Définit un autre port si 5173 est déjà utilisé
+  },
 })
